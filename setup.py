828cc87dd0003c50bfe3b3d4fa04fd83abfee9ec
Drame Zhao <jzha0634@uni.sydney.edu.au>
2025-04-24 01:19:21 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 APP = ['fetch_yahoo_finance_data.py']
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['pandas', 'yfinance', 'openpyxl', 'tkinter', 'certifi', 'jaraco.text'],
-    'includes': ['tkinter', 'jaraco.text'],
+    'packages': ['pandas', 'yfinance', 'openpyxl', 'tkinter', 'certifi'],
+    'includes': ['tkinter'],
 }
 
 setup(
